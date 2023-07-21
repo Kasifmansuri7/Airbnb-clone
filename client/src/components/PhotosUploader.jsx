34f3a1b4b8e890e36083e43136442a737b74454b
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2023-07-21 11:06:09 +0530

    photosUploader

---
+++ client/src/components/PhotosUploader.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function PhotosUploader() {
+  const [photoLink, setPhotoLink] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+
+  async function addPhotoByLink(event) {
+    event.preventDefault();
+    const { data: filename } = await axios.post("/upload-by-link", {
+      link: photoLink,
+    });
+    setAddedPhotos((prev) => {
+      return [...prev, filename];
+    });
+    setPhotoLink("");
+  }
+
+  async function uploadPhoto(e) {
+    const files = e.target.files;
+    const data = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      data.append("photos", files[i]);
+    }
+
+    await axios
+      .post("/upload", data, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((response) => {
+        const { data: filenames } = response;
+        setAddedPhotos((prev) => {
+          return [...prev, ...filenames];
+        });
+      });
+  }
+  return (
+    //UPLOAD BY LINK
+    <React.Fragment>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Add using a link  .jpeg/png"
+          value={photoLink}
+          onChange={(e) => setPhotoLink(e.target.value)}
+        />
+        <button
+          onClick={addPhotoByLink}
+          className="bg-gray-200 grow px-4 rounded-2xl "
+        >
+          Add&nbsp;photos
+        </button>
+      </div>
+
+      {/* UPLOAD LOCALLY */}
+      <div className=" mt-2 grid gap-2 grid-cols-3  lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1">
+        {addedPhotos.length > 0 && (
+          <React.Fragment>
+            {addedPhotos.map((item, index) => (
+              <img
+                key={item}
+                src={"http://localhost:3000/uploads/" + item}
+                className="rounded-2xl max-h-40 min-w-full object-cover "
+                alt="image"
+              />
+            ))}
+          </React.Fragment>
+        )}
+        <label className=" cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl py-10 px-6 text-2xl text-gray-600">
+          <input
+            type="file"
+            className="hidden"
+            multiple
+            onChange={uploadPhoto}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+            />
+          </svg>
+          Upload
+        </label>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default PhotosUploader;
--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "./Perks";
-import axios from "axios";
+import PhotosUploader from "./PhotosUploader";
 
 function PlacesPage() {
   const { action } = useParams();
   const [title, setTitle] = useState("");
   const [address, setAddress] = useState("");
-  const [addedPhotos, setAddedPhotos] = useState([]);
-  const [photoLink, setPhotoLink] = useState("");
   const [desc, setDesc] = useState("");
   const [perks, setPerks] = useState([]);
   const [extraInfo, setExtraInfo] = useState("");
@@ -24,38 +22,7 @@ function PlacesPage() {
       </React.Fragment>
     );
   }
-  //
 
-  async function addPhotoByLink(event) {
-    event.preventDefault();
-    const { data: filename } = await axios.post("/upload-by-link", {
-      link: photoLink,
-    });
-    setAddedPhotos((prev) => {
-      return [...prev, filename];
-    });
-    setPhotoLink("");
-  }
-
-  async function uploadPhoto(e) {
-    const files = e.target.files;
-    const data = new FormData();
-
-    for (let i = 0; i < files.length; i++) {
-      data.append("photos", files[i]);
-    }
-
-    await axios
-      .post("/upload", data, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-      .then((response) => {
-        const { data: filenames } = response;
-        setAddedPhotos((prev) => {
-          return [...prev, ...filenames];
-        });
-      });
-  }
   function handlePerks(name) {
     if (perks.includes(name)) {
       setPerks((prev) => prev.filter((item) => item !== name));
@@ -63,7 +30,6 @@ function PlacesPage() {
       setPerks((prev) => [...prev, name]);
     }
   }
-  console.log(perks);
   return (
     <div>
       {action !== "new" && (
@@ -112,57 +78,7 @@ function PlacesPage() {
               onChange={(e) => setAddress(e.target.value)}
             />
             {preInput("Photos", "Photos should me clear and visible.")}
-            <div className="flex gap-2">
-              <input
-                type="text"
-                placeholder="Add using a link  .jpeg/png"
-                value={photoLink}
-                onChange={(e) => setPhotoLink(e.target.value)}
-              />
-              <button
-                onClick={addPhotoByLink}
-                className="bg-gray-200 grow px-4 rounded-2xl "
-              >
-                Add&nbsp;photos
-              </button>
-            </div>
-            <div className=" mt-2 grid gap-2 grid-cols-3  lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1">
-              {addedPhotos.length > 0 && (
-                <React.Fragment>
-                  {addedPhotos.map((item, index) => (
-                    <img
-                      key={item}
-                      src={"http://localhost:3000/uploads/" + item}
-                      className="rounded-2xl max-h-40 min-w-full object-cover "
-                      alt="image"
-                    />
-                  ))}
-                </React.Fragment>
-              )}
-              <label className=" cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl py-10 px-6 text-2xl text-gray-600">
-                <input
-                  type="file"
-                  className="hidden"
-                  multiple
-                  onChange={uploadPhoto}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
-                  />
-                </svg>
-                Upload
-              </label>
-            </div>
+            <PhotosUploader />
             {preInput("Description", "Description of the place")}
             <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
             {preInput("Perks", "Select all the perks of your place.")}
